666601a690c0baf22c654a07db4c4edf150f5563
sanis maharjan <sanis.mrzan@gmail.com>
2024-06-23 10:53:41 +0545

    sanish

--- src/BankingSystemBackend/src/main/java/bankingsystem/backend/controller/AuthenticationController.java
+++ src/BankingSystemBackend/src/main/java/bankingsystem/backend/controller/AuthenticationController.java
@@ -5,6 +5,8 @@ import bankingsystem.backend.dto.JwtRequest;
 import bankingsystem.backend.entity.User;
 import bankingsystem.backend.service.JwtUserDetailService;
 import bankingsystem.backend.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,6 +22,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class AuthenticationController {
 
+    private final Logger logger= LoggerFactory.getLogger(AuthenticationController.class);
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -34,6 +38,7 @@ public class AuthenticationController {
 
     @PostMapping(value = "/login")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+        logger.info("::::: LOGGING IN:::::::::::::::");
         authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
         final String token = jwtTokenUtil.generateTokenForLogin(userDetails);
