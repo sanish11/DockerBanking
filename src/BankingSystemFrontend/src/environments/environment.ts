2a64f9e0af397deb24d5ff86c88e91de19b84336
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-07-03 11:19:15 +0545

    Update environment.ts

--- src/BankingSystemFrontend/src/environments/environment.ts
+++ src/BankingSystemFrontend/src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://localhost:8080/BankingBackend/api',
-    apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'http://localhost:8084/api',
 };
